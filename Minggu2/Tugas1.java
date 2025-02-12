861b9e1ab8c03d6463d9e2b41b83712b5a0c01f6
Celsheaamelia <celsheeamelialailaputri@gmail.com>
2025-02-12 15:50:41 +0700

    Minggu 2

---
+++ Minggu2/Tugas1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Minggu2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tugas1 {
+   public static void main(String[] args) {
+       
+       String fName = "Lisa Palombo";
+       String stuld = "123456789";
+       String stuStatus = "Active";
+       
+       System.out.println("Student Name: " + fName);
+       System.out.println("Student ID: " +stuld);
+       System.out.println("Student Status: " +stuStatus);
+       
+   } 
+}
